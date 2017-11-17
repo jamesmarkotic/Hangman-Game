a6db11d8484ca2cd6ad23e042a0aaf6c079cdeda
James Markotic <jamesmarkotic@calvisitor-10-105-181-42.calvisitor.1918.berkeley.edu>
2017-11-16 18:29:30 -0800

    made minor changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var hangWords = ['stairway to heaven' , 'black dog' , 'the ocean' , 'whole lotta love']
+
+var wins = 0;
+
+
+// Game Rules
+
+// Function to randomly pull words from array.
+function () {
+	return hangWords[Math.floor(Math.random() * hangWords.length)];
+}
+
+// Display "_" for unguessed word
+
+// Main Function
+// Code for key press to start game
+
+document.onkeyup = function (event) {
+	if ()
+};
